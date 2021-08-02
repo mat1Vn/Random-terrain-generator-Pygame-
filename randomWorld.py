e2690700af58ea5547e63dfe533fbfdf1a31837a
Matheus Vinicius <72578489+codin01@users.noreply.github.com>
2021-08-02 14:34:09 -0300

    Add files via upload

---
+++ randomWorld.py
@@ -0,0 +1,46 @@
+from world import wd
+import pygame
+from random import randint
+
+
+pygame.init()
+screen = pygame.display.set_mode((300, 300))
+clock = pygame.time.Clock()
+fps=60
+
+data = []
+
+largura = 60
+altura = 6
+spriteSize = 40
+
+def generate():
+	global data
+	for i in range(0, largura):
+		alt = range(1, randint(altura-2, altura))
+		row = []
+		for i in alt:
+			if i == alt[-1]:
+				row.append(1)
+			else:
+				row.append(2)
+		data.append(row)	
+
+generate()				
+												
+world = wd(screen, data, spriteSize)
+
+while True:
+	screen.fill((0,0,0))
+	clock.tick(fps)
+	world.draw(True)
+	
+	point = pygame.mouse.get_pos()
+	reset = pygame.draw.rect(screen, (0,255,0), (650, 1100,40,40))
+	
+	if reset.collidepoint(point):
+		data = []
+		generate()
+		world = wd(screen, data, spriteSize)
+		
+	pygame.display.update()
---
+++ world.py
@@ -0,0 +1,46 @@
+import pygame
+
+class wd():
+	def __init__(self, screen, data, size, flip=False):
+		self.tile_list = []
+		self.size=size
+		self.screen = screen
+		self.flip = flip
+		#load images
+		pedra = pygame.image.load("res/tijolo_pedra.png")
+		grama = pygame.image.load("res/grama.png")
+		terra = pygame.image.load("res/terra.png")
+		
+		row_count = 0
+		for row in data:
+			col_count = 0
+			for tile in row:
+				if tile == 1:
+					img = pygame.transform.scale(grama, (self.size, self.size))
+					img_rect = img.get_rect()
+					img_rect.x = col_count * self.size
+					img_rect.y = row_count * self.size
+					tile = (img, img_rect)
+					self.tile_list.append(tile)
+
+				if tile == 2:
+					img = pygame.transform.scale(terra, (self.size, self.size))
+					img_rect = img.get_rect()
+					img_rect.x = col_count * self.size
+					img_rect.y = row_count * self.size
+					
+					tile = (img, img_rect)
+					self.tile_list.append(tile)
+					
+				else:
+					pass
+				col_count += 1
+			row_count += 1
+			
+	def draw(self, flip=False):
+		for tile in self.tile_list:
+			if flip:
+				img_flip = image = pygame.transform.rotate(tile[0], 270)
+				self.screen.blit(img_flip, tile[1])
+			else:
+				self.screen.blit(tile[0], tile[1])
